de42794bd598ca88816e9ac860696dde03fe05e8
Kyeza Arnold <kyezaarnold63@gmail.com>
2017-05-23 18:07:09 +0300

    FInish third nav

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
-	$("#close-btn-custom").click(function() {
+	$("#close-btn-custom").click(function () {
 		$("#navbar-custom").remove();
+	});
+});
+
+$(document).ready(function(){
+	$("#onHover").hover(function () {
+		$("#go").hide(1000);
+	});
+});
+
+$(document).ready(function(){
+	$(".dropdown-content").hover(function () {
+		$("#go").hide();
+	});
+});
+
+$(document).ready(function(){
+	$("#onHover").mouseleave(function () {
+		$("#go").show();
+	});
+});
+
+$(document).ready(function(){
+	$(".dropdown-content").mouseleave(function () {
+		$("#go").show(1000);
 	});
 });
